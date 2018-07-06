26ee5ad1de9747ae72ea2b040fb18dcd7181a060
Jatin Katyal <jatin.katyal13@gmail.com>
2018-07-06 11:45:05 +0530

    ROI addeD

--- edge.py
+++ edge.py
@@ -4,22 +4,32 @@ import cv2
 cam = cv2.VideoCapture(0)
 
 while 1:
-    # Take each frame
-    ret, frame = cam.read()
+	# Take each frame
+	ret, frame = cam.read()
 
-    if ret: 
-        # Convert to HSV for simpler calculations
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            
-        canny = cv2.Canny(frame,100,200)
-        
-        cv2.imshow('cannyh', canny)
+	if ret: 
+		# Convert to HSV for simpler calculations
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
+		canny = cv2.Canny(frame,100,200)
+
+		blur = cv2.GaussianBlur(canny, (41, 41), 0)
+
+		(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(blur)
+
+		image = frame.copy()
+
+		roi=image[maxLoc[1]-250:maxLoc[1]+250,maxLoc[0]-250:maxLoc[0]+250,2:2]
+			
+		cv2.imshow('cannyh', canny)
+
+		cv2.imshow('roi', roi)
+
+	k = cv2.waitKey(5) & 0xFF
+	if k == 27:
+		break
  
 cv2.destroyAllWindows()
  
 #release the frame
-cap.release()
+cam.release()
